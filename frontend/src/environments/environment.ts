57119dbf6fe2cc81e119b9b1a5a2ccee2829e6af
Guilherme Bispo <guibispo@gmail.com>
2025-10-23 23:39:36 -0300

    Configure local stack defaults

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // baseUrl : 'http://localhost:8080'
-  baseUrl: 'https://hospital-inteligente-backend.onrender.com'
+  baseUrl : 'http://localhost:8080'
 };
